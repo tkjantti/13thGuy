e54a1cee319ced5bf3ef8cf5c1106b1eef117dff
Tero Jäntti <tkjantti@gmail.com>
2024-09-20 19:19:19 +0300

    Removed roadroller and package size check.

--- createDeployPackage.js
+++ createDeployPackage.js
@@ -11,16 +11,9 @@ let archive = archiver("zip", {
     zlib: { level: 9 }, // set compression to best
 });
 
-const MAX = 13 * 1024; // 13kb
-
 output.on("close", function () {
     const bytes = archive.pointer();
-    const percent = ((bytes / MAX) * 100).toFixed(2);
-    if (bytes > MAX) {
-        console.error(`Size overflow: ${bytes} bytes (${percent}%)`);
-    } else {
-        console.log(`Size: ${bytes} bytes (${percent}%)`);
-    }
+    console.log(`Size: ${bytes} bytes`);
 });
 
 archive.on("warning", function (err) {
